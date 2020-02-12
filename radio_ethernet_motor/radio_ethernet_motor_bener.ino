0900769a0675b6b7c1c7c391e0c9fbc275cbbad6
iqbaluhuy <56419725+iqbaluhuy@users.noreply.github.com>
2020-02-12 17:21:30 +0700

    Update radio_ethernet_motor_bener.ino

--- radio_ethernet_motor/radio_ethernet_motor_bener.ino
+++ radio_ethernet_motor/radio_ethernet_motor_bener.ino
@@ -1,14 +1,14 @@
 /*
   Web client
- This sketch connects to a website (http://www.google.com)
- using an Arduino Wiznet Ethernet shield.
- Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe, based on work by Adrian McEwen
- */
+  This sketch connects to a website (http://www.google.com)
+  using an Arduino Wiznet Ethernet shield.
+  Circuit:
+   Ethernet shield attached to pins 10, 11, 12, 13
+  created 18 Dec 2009
+  by David A. Mellis
+  modified 9 Apr 2012
+  by Tom Igoe, based on work by Adrian McEwen
+*/
 
 #include <SPI.h>
 #include <Ethernet.h>
@@ -23,6 +23,7 @@ byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
 //IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+//char server[] = "172.20.17.254";    // name address for Google (using DNS)
 char server[] = "www.korlantas.id";    // name address for Google (using DNS)
 const char* phpinsert = " /motor.php";
 const char* phpselect = " /selectmotor.php";
@@ -31,10 +32,10 @@ const int port   = 80;
 String nocones;
 String statuss;
 String tegangan;
-String datamotor,datanya;
-String respon,respon1,respon2,respon3,respon4,respon5,finish,belok,s,sub;
-int panjang,panjang1,panjang2,panjang3,panjang4,panjang5;
-bool m1,m2,m3,m4,mm1,mm2,mm3,mm4,m5,rc,bkanan,bkiri;
+String datamotor, datanya;
+String respon, respon1, respon2, respon3, respon4, respon5, finish, belok, s, sub;
+int panjang, panjang1, panjang2, panjang3, panjang4, panjang5;
+bool m1, m2, m3, m4, mm1, mm2, mm3, mm4, m5, rc, bkanan, bkiri;
 char cc[2];
 
 // Set the static IP address to use if the DHCP fails to assign
@@ -109,23 +110,23 @@ void loop() {
   // if there are incoming bytes available
   // from the server, read them and print them:
   int len = client.available();
-// if (len) {
-//    byte buffer[250];
-//     char c[250];
-//    if (len > 250) len = 250;
-//    client.read(c, 250);
-//    if (printWebData) {
-////      Serial.write(c, 246); // show in the serial monitor (slows some boards)
-//      String data = String(c[250]);
-//      String datakirim = data.substring(246,247);
-////      Serial.println(data);
-//      Serial.write(c[246]);
-//      char cdatakirim[2];
-//      datakirim.toCharArray(cdatakirim,2);
-//      HC12.write(c[246]);
-//    }
-//    byteCount = byteCount + len;
-//  }
+  if (len) {
+    byte buffer[250];
+    char c[250];
+    if (len > 250) len = 250;
+    client.read(c, 250);
+    if (printWebData) {
+      //      Serial.write(c, 246); // show in the serial monitor (slows some boards)
+      String data = String(c[250]);
+      String datakirim = data.substring(246, 247);
+      //      Serial.println(data);
+      Serial.write(c[246]);
+      char cdatakirim[2];
+      datakirim.toCharArray(cdatakirim, 2);
+      HC12.write(c[246]);
+    }
+    byteCount = byteCount + len;
+  }
 
   // if the server's disconnected, stop the client:
   if (!client.connected()) {
@@ -145,59 +146,60 @@ void loop() {
     Serial.println();
   }
 
-    // do nothing forevermore:
-        while (HC12.available()) 
-    {
-      char mot = HC12.read();
-      datamotor += mot;
-    }
-//      Serial.println(datamotor);
-//      
-//      if (datamotor.startsWith("<"))
-//      {
-      if(datamotor.length() > 20)
-      {
-        if (client.connect(server, 80)) 
-        {
-          int indexx = datamotor.indexOf('<');
-          int respon = datamotor.indexOf('>',indexx);
-          datanya = datamotor.substring(indexx + 1, respon);
-        Serial.println(datanya);
-         Serial.print("connected to ");
-         Serial.println(client.remoteIP());
-      // Make a HTTP request:
-         String url; 
-         url += phpinsert;
-         url += "?motor=";
-         url += datanya;
-   
-         client.print("GET"); // /chiller.php?mood=najib_ganteng
-         client.print(url);
-         client.println(" HTTP/1.1");
-         client.println("Host: korlantas.id");
-         client.println("Connection: close");
-         client.println();
-         Serial.println(url);
-         }
-        len = client.available();
-   if (len) {
-    byte buffer[250];
-     char c[250];
-    if (len > 250) len = 250;
-//    client.read(c, 250);
-    if (printWebData) {
-//      Serial.write(c, 246); // show in the serial monitor (slows some boards)
-      String data = String(c[249,250]);
-      String datakirim = data.substring(246,247);
-      Serial.write(c[246]);
-      HC12.write(c[246]);
-//      HC12.println(datakirim);
-    }
-    byteCount = byteCount + len;
+  // do nothing forevermore:
+  while (HC12.available())
+  {
+    char mot = HC12.read();
+    datamotor += mot;
   }
-    datamotor = "";
-//    datanya="";
-    delay(600);
+  //      Serial.println(datamotor);
+  //
+if (datamotor.startsWith("<"))
+{
+  if (datamotor.length() > 20)
+  {
+    if (client.connect(server, 80))
+    {
+      int indexx = datamotor.indexOf('<');
+      int respon = datamotor.indexOf('>', indexx);
+      datanya = datamotor.substring(indexx + 1, respon);
+      Serial.println(datanya);
+      Serial.print("connected to ");
+      Serial.println(client.remoteIP());
+      // Make a HTTP request:
+      String url;
+      url += phpinsert;
+      url += "?motor=";
+      url += datanya;
+
+      client.print("GET"); // /chiller.php?mood=najib_ganteng
+      client.print(url);
+      client.println(" HTTP/1.1");
+      client.println("Host: korlantas.id");
+      client.println("Connection: close");
+      client.println();
+      Serial.println(url);
     }
+    len = client.available();
+    //   if (len) {
+    //    byte buffer[250];
+    //     char c[250];
+    //    if (len > 250) len = 250;
+    ////    client.read(c, 250);
+    //    if (printWebData) {
+    ////      Serial.write(c, 246); // show in the serial monitor (slows some boards)
+    //      String data = String(c[249,250]);
+    //      String datakirim = data.substring(246,247);
+    //      Serial.write(c[246]);
+    //      HC12.write(c[246]);
+    ////      HC12.println(datakirim);
+    //    }
+    //    byteCount = byteCount + len;
+      }
+      
+    datamotor = "";
+    //    datanya="";
+    delay(100);
+  }
 
 }
